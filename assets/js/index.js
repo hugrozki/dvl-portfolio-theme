139fd323fc82d2d9082b467bc0c6f651bd5429a3
Hugo Acosta <hugrozki@gmail.com>
2023-02-21 12:59:30 -0600

    add transitions state class for load, add page navigator component

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 (function (d) {
-  const $navIcon = d.querySelector(".navigation-navicon");
-  const $navLinks = d.querySelector(".navigation-links");
+  const $navIcon = d.querySelector(".navigation-navicon"),
+    $navLinks = d.querySelector(".navigation-links"),
+    $fadeInEl = d.querySelector(".fadeIn"),
+    $transitionEl = d.querySelector(".v-transition");
 
   $navIcon.addEventListener("click", (e) => {
     $navLinks.classList.toggle("is-visible");
@@ -9,5 +11,17 @@
   d.addEventListener("click", (e) => {
     if (e.target.matches(".navigation-navicon")) return false;
     $navLinks.classList.remove("is-visible");
+  });
+
+  window.addEventListener("load", () => {
+    d.body.classList.remove("opacity");
+
+    if ($fadeInEl) {
+      $fadeInEl.style.opacity = "1";
+    }
+
+    if ($transitionEl) {
+      $transitionEl.style.transform = "translateY(0)";
+    }
   });
 })(document);
